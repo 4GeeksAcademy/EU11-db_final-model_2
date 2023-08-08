7bd19806b699ca01c02cb5f52907fa95ee0c28fc
bjorkrothjm <64375352+bjorkrothjm@users.noreply.github.com>
2023-08-08 21:14:40 +0000

    Added 2 different models testing Educatives method and the method used in the original starwars models.py

--- src/models.py
+++ src/models.py
@@ -5,28 +5,94 @@ from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
+# From Educative
+# from flask_sqlalchemy import SQLAlchemy
+# db = SQLAlchemy(app)
+
+
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+# MODEL 1: s-model | Version: Educative 
+# A Favorite can belong only to 1 User  [An Employee can be assiciated with only 1 Department] > FK on Favorite table
+# A User can have many Favorites [A Department can have multiple Employees] > relationship on User table 
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Users(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    user_name = Column(String(250), nullable=False)
+    email = Column(String(250), unique=True, nullable=False)
+    password = Column(String(250), nullable=False)
+    # Added
+    favorites = relationship('Favorites', backref = 'users')
 
-    def to_dict(self):
-        return {}
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    #Added
+    users_id = Column(String, ForeignKey('users.id'), nullable = False)
+
+    # stadiums = relationship('Stadiums', backref = 'favorites')
+    # user_id = Column(Integer, ForeignKey("user.id"))
+    # stadium_id = Column(Integer, ForeignKey("stadium.id"))
+    # stadiums = relationship('Stadiums')
+    # users = relationship('User')
+
+# MODEL 2 | Version: Star wars template 
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    user_name = Column(String(250), nullable=False)
+    email = Column(String(250), unique=True, nullable=False)
+    password = Column(String(250), nullable=False)
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)  
+    users = relationship('User', backref = 'favorite')
+    
+
+
+# class Visisted(Base):
+#     __tablename__ = 'visited'
+#     id = Column(Integer, primary_key=True)
+#     user_id = Column(Integer, ForeignKey("user.id"))
+#     stadium_id = Column(Integer, ForeignKey("stadium.id"))
+
+#     # stadiums = relationship('Stadiums')
+#     # users = relationship('User')
+
+# class Reviews(Base):
+#     __tablename__ = 'reviews'
+#     id = Column(Integer, primary_key=True)
+#     user_id = Column(Integer, ForeignKey("user.id"))
+#     stadium_id = Column(Integer, ForeignKey("stadium.id"))
+#     review_rating = Column(Integer, nullable=False)
+#     review_text = Column(String(1000), nullable=False)
+#     # stadiums = relationship('Stadiums')
+#     # users = relationship('User')
+
+# class Stadiums(Base):
+#     __tablename__ = 'stadiums'
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(250))
+#     address = Column(String(250))
+#     city = Column(String(250))
+#     country = Column(String(250))
+#     surface = Column(String(250))
+#     image = Column(String(250))
+#     # favorites = Column(Integer, ForeignKey('favorites.id'))
+#     # reviews = Column(Integer, ForeignKey('reviews.id'))
+#     # visited = Column(Integer, ForeignKey('visited.id'))
+#     # reviews = Column(Integer, ForeignKey('review.id'))
+
+
+
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
